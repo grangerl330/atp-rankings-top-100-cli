77fd8b8138d77a6b894baaeded661d11e9cc2099
Luke Granger <grangerl330@gmail.com>
2019-02-05 04:41:04 +0000

    added methods to cli.rb and updated start method

--- lib/atp_rankings_top_100_cli/cli.rb
+++ lib/atp_rankings_top_100_cli/cli.rb
@@ -8,7 +8,7 @@ class AtpRankingsTop100Cli::CLI
   def start
     range_choice
     if @@input > 0
-      range_choice
+      list_range(@@input)
       menu
     else
       puts "Invalid Input"
@@ -29,6 +29,34 @@ class AtpRankingsTop100Cli::CLI
     puts "81-90"
     puts "91-100"
     @@input = gets.strip.to_i
+  end
+
+  def list_range(input)
+    if (1..10).include?(input)
+      AtpRankings::Player.all[0..9].each_with_index {|player, index| puts "#{index+1}. #{player.name}"}
+    elsif (11..20).include?(input)
+      AtpRankings::Player.all[10..19].each_with_index {|player, index| puts "#{index+1}. #{player.name}"}
+    elsif (21..30).include?(input)
+      AtpRankings::Player.all[20..29].each_with_index {|player, index| puts "#{index+1}. #{player.name}"}
+    elsif (31..40).include?(input)
+      AtpRankings::Player.all[30..39].each_with_index {|player, index| puts "#{index+1}. #{player.name}"}
+    elsif (41..50).include?(input)
+      AtpRankings::Player.all[40..49].each_with_index {|player, index| puts "#{index+1}. #{player.name}"}
+    elsif (51..60).include?(input)
+      AtpRankings::Player.all[50..59].each_with_index {|player, index| puts "#{index+1}. #{player.name}"}
+    elsif (61..70).include?(input)
+      AtpRankings::Player.all[60..69].each_with_index {|player, index| puts "#{index+1}. #{player.name}"}
+    elsif (71..80).include?(input)
+      AtpRankings::Player.all[70..79].each_with_index {|player, index| puts "#{index+1}. #{player.name}"}
+    elsif (81..90).include?(input)
+      AtpRankings::Player.all[80..89].each_with_index {|player, index| puts "#{index+1}. #{player.name}"}
+    elsif (91..100).include?(input)
+      AtpRankings::Player.all[90..99].each_with_index {|player, index| puts "#{index+1}. #{player.name}"}
+    end
+  end
+
+  def menu
+    puts "Enter player name or number for more information:"
   end
 
 end
