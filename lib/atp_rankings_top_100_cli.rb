3ffe92adb9aa69b5cba472c1af6ea2113939efc2
Luke Granger <grangerl330@gmail.com>
2019-02-05 04:08:59 +0000

    created config and environment, updated dependencies

--- lib/atp_rankings_top_100_cli.rb
+++ lib/atp_rankings_top_100_cli.rb
@@ -1,6 +1,4 @@
-require "atp_rankings_top_100_cli/version"
-
 module AtpRankingsTop100Cli
-  class Error < StandardError; end
-  # Your code goes here...
 end
+
+require_relative '../config/environment'
