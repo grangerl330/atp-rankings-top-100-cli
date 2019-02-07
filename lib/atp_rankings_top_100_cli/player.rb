4b141b0b880226da780e0bfb5f07997586ad4a02
Luke Granger <grangerl330@gmail.com>
2019-02-07 01:23:07 +0000

    added attributes and methods to player class

--- lib/atp_rankings_top_100_cli/player.rb
+++ lib/atp_rankings_top_100_cli/player.rb
@@ -1,5 +1,5 @@
 class AtpRankingsTop100Cli::Player
-  attr_accessor :name, :rank, :age, :points, :country, :num_tourns_played, :bio_link
+  attr_accessor :name, :rank, :age, :points, :country, :num_tourns_played, :bio_link, :turned_pro, :weight, :height, :birthplace, :residence, :plays, :coach
 
   @@all = []
 
@@ -13,6 +13,42 @@ class AtpRankingsTop100Cli::Player
 
   def self.list(start_of_range, end_of_range)
     @@all[start_of_range..end_of_range].each_with_index {|player, index| puts "#{index + start_of_range + 1}. #{player.name}"}
+  end
+
+  def self.find(id)
+    @@all[id-1]
+  end
+
+  def doc
+    @doc ||= Nokogiri::HTML(open(self.bio_link))
+  end
+
+  def turned_pro
+    @turned_pro = doc.css(".table-big-value")[1].text.strip
+  end
+
+  def weight
+    @weight = doc.css(".table-big-value")[2].text.strip
+  end
+
+  def height
+    @height = doc.css(".table-big-value")[3].text.strip
+  end
+
+  def birthplace
+    @birthplace = doc.css(".table-value")[0].text.strip
+  end
+
+  def residence
+    @residence = doc.css(".table-value")[1].text.strip
+  end
+
+  def plays
+    @plays = doc.css(".table-value")[2].text.strip
+  end
+
+  def coach
+    @coach = doc.css(".table-value")[3].text.strip
   end
 
 end
